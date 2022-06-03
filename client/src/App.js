3c0ce0fbaede6e37172e451d970edf315375d846
houssembouraoui <houssembouraoui6@gmail.com>
2022-06-03 02:29:15 +0100

    admin page added but request blocked by corsd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import Footer from "./layouts/footer.jsx";
 import AdminAuth from "./components/AdminAuth.jsx";
 import Home from "./components/home.jsx";
 import LogIn from "./components/login.jsx";
@@ -8,12 +7,13 @@ import NavBar from "./layouts/navBar.jsx";
 import AddUser from "./components/addUser.jsx";
 import axios from "axios";
 import AdminPage from "./components/adminPage.jsx";
-import AdminNav from "./layouts/adminNav.jsx";
 import Analyse from "./components/analyse.jsx";
 import UserNav from "./layouts/userNav.jsx";
 import Nav from "./layouts/newNav.jsx";
 import Test from "./components/contextTest.jsx";
 import { Context } from "./components/contextBidou.js";
+import NewAdminNav from "./layouts/adminNav.jsx";
+import NewFooter from "./layouts/newFooter.jsx";
 
 export const MyContext = React.createContext();
 
@@ -27,11 +27,9 @@ const App = () => {
 
   return (
     <>
-      {/* <MyContext.Provider value={test}> */}
-      {/* <Nav /> */}
       <Test />
       {view.includes("admin") ? (
-        <AdminNav view={view} changeView={changeView} />
+        <NewAdminNav view={view} changeView={changeView} />
       ) : (
         <NavBar view={view} changeView={changeView} />
       )}
@@ -46,9 +44,7 @@ const App = () => {
       )}
 
       {view === "analyse" && <Analyse />}
-
-      <Footer changeView={changeView} />
-      {/* </MyContext.Provider> */}
+      <NewFooter changeView={changeView} />
     </>
   );
 };
