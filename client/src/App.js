80f7336de51ba9acc3ddc7c0db9e6fd2f59336d2
houssembouraoui <houssembouraoui6@gmail.com>
2022-06-06 00:38:37 +0100

    routes but still working on links

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AdminAuth from "./components/AdminAuth.jsx";
 import Home from "./components/home.jsx";
@@ -18,6 +19,7 @@ import CamionPage from "./components/receptorAgent/CamionPage.jsx";
 import NewAdminNav from "./layouts/adminNav.jsx";
 import NewFooter from "./layouts/newFooter.jsx";
 import AdminLogin from "./components/AdminLogin.jsx";
+import NewUserLogin from "./components/newUserLogin.jsx";
 
 export const MyContext = React.createContext();
 
@@ -33,25 +35,38 @@ const App = () => {
     <>
       <MyContext.Provider value={test}>
         <Header />
+        <NewAdminNav changeView={changeView} />
         {/* <CamionPage /> */}
         {view.includes("admin") ? (
           <NewAdminNav view={view} changeView={changeView} />
         ) : (
           <NavBar view={view} changeView={changeView} />
         )}
-        {view === "home" && <Home />}
-        {view === "login" && <LogIn changeView={changeView} />}
-        {view === "adminLogIn" && (
-          <AdminLogin changeView={changeView} view={view} />
-        )}
-        {view === "admin add" && <AddUser />}
-        {view === "admin page" && (
-          <AdminPage changeView={changeView} view={view} />
-        )}
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/userLogin"
+              element={<LogIn changeView={changeView} />}
+            />
 
-        {view === "analyse" && <Analyse />}
+            <Route
+              exact
+              path="/adminLogin"
+              element={<AdminLogin changeView={changeView} view={view} />}
+            />
+            <Route exact path="addUser" element={<AddUser />} />
 
-        <NewAdminNav changeView={changeView} />
+            <Route
+              exact
+              path="/admin"
+              element={<AdminPage changeView={changeView} view={view} />}
+            />
+            <Route exact path="/analyse" element={<Analyse />} />
+          </Routes>
+        </BrowserRouter>
+        <NewFooter changeView={changeView} />
       </MyContext.Provider>
     </>
   );
